3b52d9aa1585b4a03acacdb8715eaf51b2421ae0
Harsh2438 <ht2081148@gmail.com>
2026-05-12 22:21:33 +0530

    Added Docker support

--- umsproject/umsproject/settings.py
+++ umsproject/umsproject/settings.py
@@ -78,9 +78,11 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
